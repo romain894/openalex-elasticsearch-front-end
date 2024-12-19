37722864716d1bda7bf45a8e5b366151f9ab4a3c
Romain THOMAS <38260500+romain894@users.noreply.github.com>
2024-12-19 01:35:09 +0100

    removed flowbite react lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import {useState, useEffect} from 'react'
 
-import { TextInput } from "flowbite-react";
-
 import './App.css'
 import Work from './components/work.jsx';
 
@@ -35,8 +33,28 @@ function App() {
 
     return (
         <>
-            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">Search for <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Works</span> with AI</h1>
-            <TextInput className="mb-6" placeholder="Search" onChange={event => search_works(event.target.value)}/>
+            <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">Search
+                for <span
+                    className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Works</span> with AI</h1>
+
+
+
+            <form className="mb-4 max-w-xl mx-auto">
+                <label htmlFor="default-search" className="mb-6 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </div>
+                    <input onChange={event => search_works(event.target.value)} type="search" id="work-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-teal-400 focus:border-teal-400 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500" placeholder="Search Works" required />
+                    {/*<button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-gradient-to-r from-sky-400 to-emerald-600 hover:from-sky-500 hover:to-emerald-700 focus:ring-4 focus:outline-none focus:ring-teal-400 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gradient-to-r dark:from-sky-400 dark:to-emerald-600 dark:hover:from-sky-500 dark:hover:to-emerald-700 dark:focus:ring-teal-800">Search</button>*/}
+                </div>
+            </form>
+
+
+
+            {/*<TextInput className="mb-6" placeholder="Search" onChange={event => search_works(event.target.value)}/>*/}
 
             <section className="">
                 {works.map((work) =>
--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,27 +1,23 @@
-import {Card, Button} from "flowbite-react";
-
 export default function Work(props) {
     return (
-        <Card className="mb-4 flex flex-col justify-center min-w-full">
-            <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <div
+            className="mb-4 max-w-3xl mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
                 {props.title}
             </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400 text-justify">
+            <p className="mb-4 font-normal text-gray-700 dark:text-gray-400 text-justify">
                 {props.abstract}
             </p>
-            <div className="flex items-center justify-center min-w-full">
-                <Button as="a" href={props.id} gradientDuoTone="greenToBlue" className="max-w-xl">
-                    {props.id}
-                    <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20"
-                         xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            fillRule="evenodd"
-                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
-                </Button>
-            </div>
-        </Card>
+            <a href={props.id}
+               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gradient-to-r from-sky-400 to-emerald-600 hover:from-sky-500 hover:to-emerald-700 focus:ring-4 focus:outline-none focus:ring-teal-400 rounded-lg 2 dark:bg-gradient-to-r dark:from-sky-400 dark:to-emerald-600 dark:hover:from-sky-500 dark:hover:to-emerald-700 dark:focus:ring-teal-800">
+                {props.id}
+                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                     fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                          d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+        </div>
+
     )
 }
